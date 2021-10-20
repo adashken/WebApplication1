6589eb7d6d6b88d34577f94af59200e1eff3f81a
dindan8938@gmail.com <dindan8938@gmail.com>
2021-10-20 18:44:46 +0300

    Add project files.

---
+++ WebApplication1/Data/FlavorContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+    public class FlavorContext : DbContext
+    {
+        public FlavorContext (DbContextOptions<FlavorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Flavor> Flavor { get; set; }
+    }
---
+++ WebApplication1/Data/OrderContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+    public class OrderContext : DbContext
+    {
+        public OrderContext (DbContextOptions<OrderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Order> Order { get; set; }
+    }
